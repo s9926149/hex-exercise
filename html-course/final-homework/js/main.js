c3b201aed42b8f1facac70fc46e725d780262fab
El Che <s9926149@gmail.com>
2020-07-14 18:13:31 +0800

    [jQuery final homework] Adopt swiper
    Apply sipwer slide effect on banner

--- html-course/final-homework/js/main.js
+++ html-course/final-homework/js/main.js
@@ -2,3 +2,17 @@
 lightbox.option({
     positionFromTop: 200,
 });
+
+$(function () {
+    var mySwiper = new Swiper('.swiper-container', {
+        loop: true,
+        
+        pagination: {
+            el: '.swiper-pagination',
+        },
+
+        autoplay: {
+            delay: 3000,
+        },
+    });
+});
