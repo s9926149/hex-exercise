bf0988024d2357e3c2486f0947168d29c98f90db
El Che <s9926149@gmail.com>
2020-07-15 16:20:01 +0800

    [jQuery final homework] Adopt fontawesome
    Add scroll to top butoon
    Restructure misplaced header and main closing tag

--- html-course/final-homework/js/main.js
+++ html-course/final-homework/js/main.js
@@ -1,9 +1,13 @@
-// lightbox settings
-lightbox.option({
-    positionFromTop: 200,
-});
+
+
 
 $(function () {
+    // lightbox settings
+    lightbox.option({
+        positionFromTop: 200,
+    });
+
+    // setup swiper
     var mySwiper = new Swiper('.swiper-container', {
         loop: true,
         
@@ -14,5 +18,11 @@ $(function () {
         autoplay: {
             delay: 3000,
         },
+    });
+
+    // setup scroll to top button
+    $('.toTop').click(function (event) {
+        event.preventDefault();
+        $('html,body').animate({ scrollTop: 0}, 750);
     });
 });
